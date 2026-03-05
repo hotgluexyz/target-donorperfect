18c0fa9fc566318bec5ef112a9aabd7e7e4b42b9
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2026-03-04 19:06:34 -0500

    HGI-9853: add support to send clicked, opened and bounced emails (#2)

--- target_donorperfect/sinks.py
+++ target_donorperfect/sinks.py
@@ -107,7 +107,8 @@ class ContactsSink(DonorPerfectSink):
             "@comment": record.get("comment", ""),
             "@document_path": record.get("document_path", ""),
             "@user_id": record.get("user_id", ""),
-            "@contact_email": record.get("contact_email", "")
+            "@contact_email": record.get("contact_email", ""),
+            "@em_campaign_status": record.get("em_campaign_status", "")
             }.items()
         }
         params["params"] = ",".join([f"{k}={v}" if not isinstance(v, str) else f"{k}='{v}'" for k, v in fields.items()])
