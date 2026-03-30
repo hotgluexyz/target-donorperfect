14497935be847085afdc75e3680797097e6cc457
Azmat Ali <mian.azmatali.9@gmail.com>
2026-03-31 03:51:46 +0500

    HGI-10018: Handle contact updates properly and rename sink (#4)

--- target_donorperfect/sinks.py
+++ target_donorperfect/sinks.py
@@ -24,6 +24,10 @@ class DonorsSink(DonorPerfectSink):
             # add donor_id to existing record for state, updates always return donor_id 0
             params["donor_id"] = existing_record.get("donor_id", 0)
 
+            if record.get("email_status") != existing_record.get("email_status"):
+                params["updated_email_status"] = record.get("email_status") or ""
+                params["email_status_date"] = record.get("email_status_date") or ""
+
         # fill empty values with existing values
         existing_record.update(record)
         # process data
@@ -56,6 +60,8 @@ class DonorsSink(DonorPerfectSink):
                 "@donor_type": existing_record.get("donor_type", ""),
                 "@nomail": existing_record.get("nomail", ""),
                 "@nomail_reason": existing_record.get("nomail_reason", ""),
+                "@email_status": existing_record.get("email_status", ""),
+                "@email_status_date": existing_record.get("email_status_date", ""),
                 "@narrative": existing_record.get("narrative", ""),
                 "@donor_rcpt_type": existing_record.get("donor_rcpt_type", ""),
                 "@user_id": existing_record.get("user_id", ""),
@@ -69,50 +75,71 @@ class DonorsSink(DonorPerfectSink):
     def upsert_record(self, record: dict, context: dict) -> None:
         """Upsert the record."""
         method = "GET"
+        state_updates = dict()
 
         # get donor_id for updates
         donor_id = record.pop("donor_id", None)
+        updated_email_status = record.pop("updated_email_status", None)
+        email_status_date = record.pop("email_status_date", None)
 
         # send request
         response = self.request_api(method, params=record)
         res_json = self.parse_xml_response(response.text)
-        id = donor_id or res_json.get("", None)
-        return id, True, dict()
+        
+        if updated_email_status is not None:
+            safe_status = self.escape_single_quotes(updated_email_status)
+            safe_date = self.escape_single_quotes(email_status_date)
+            update_query = f"UPDATE DPADDRESS SET email_status='{safe_status}', email_status_date='{safe_date}' WHERE donor_id='{donor_id}'"
+            self.request_api("GET", params={"action": update_query})
+
+        if donor_id:
+            state_updates['is_updated'] = True
+            return donor_id, True, state_updates
+
+        id = res_json.get("", None)
+        return id, True, state_updates
 
 
 class ContactsSink(DonorPerfectSink):
     """DonorPerfect target sink class."""
 
-    name = "contacts"
+    name = "dp_contacts"
 
     def preprocess_record(self, record: dict, context: dict) -> None:
         """Process the record."""
-        
-        # process data
         params = {}
+        existing_record = {}
+
+        if record.get("contact_id", None):
+            response = self.request_api("GET", params={"action": f"select * FROM dpcontact WHERE contact_id='{record['contact_id']}'", "apikey": unquote(self.config.get("api_token"))})
+            existing_record = self.parse_xml_response(response.text)
+            # add contact_id to existing record for state, updates always return contact_id 0
+            params["contact_id"] = existing_record.get("contact_id", 0)
+
+        # fill empty values with existing values
+        existing_record.update(record)
 
         params["action"] = "dp_savecontact"
-        # process record fields
         fields = {
             k: self.escape_single_quotes(v) for k, v in {
-            "@contact_id": record.get("contact_id", 0),
-            "@donor_id": record.get("donor_id", ""),
-            "@activity_code": record.get("activity_code", ""),
-            "@mailing_code": record.get("middle_name", ""),
-            "@by_whom": record.get("by_whom", ""),
-            "@contact_date": record.get("contact_date", ""),
-            "@due_date": record.get("due_date", ""),
-            "@due_time": record.get("due_time", ""),
-            "@completed_date": record.get("completed_date", ""),
-            "@comment": record.get("comment", ""),
-            "@document_path": record.get("document_path", ""),
-            "@user_id": record.get("user_id", ""),
-            "@contact_email": record.get("contact_email", ""),
-            "@em_campaign_status": record.get("em_campaign_status", ""),
-            "@em_campaign": record.get("em_campaign", ""),
-            "@em_event_status_date": record.get("em_event_status_date", ""),
-            "@em_bounce_reason": record.get("em_bounce_reason", ""),
-            "@contact_state": record.get("contact_state", "")
+            "@contact_id": existing_record.get("contact_id", 0),
+            "@donor_id": existing_record.get("donor_id", ""),
+            "@activity_code": existing_record.get("activity_code", ""),
+            "@mailing_code": existing_record.get("mailing_code", ""),
+            "@by_whom": existing_record.get("by_whom", ""),
+            "@contact_date": existing_record.get("contact_date", ""),
+            "@due_date": existing_record.get("due_date", ""),
+            "@due_time": existing_record.get("due_time", ""),
+            "@completed_date": existing_record.get("completed_date", ""),
+            "@comment": existing_record.get("comment", ""),
+            "@document_path": existing_record.get("document_path", ""),
+            "@user_id": existing_record.get("user_id", ""),
+            "@contact_email": existing_record.get("contact_email", ""),
+            "@em_campaign_status": existing_record.get("em_campaign_status", ""),
+            "@em_campaign": existing_record.get("em_campaign", ""),
+            "@em_event_status_date": existing_record.get("em_event_status_date", ""),
+            "@em_bounce_reason": existing_record.get("em_bounce_reason", ""),
+            "@contact_state": existing_record.get("contact_state", "")
             }.items()
         }
         params["params"] = ",".join([f"{k}={v}" if not isinstance(v, str) else f"{k}='{v}'" for k, v in fields.items()])
@@ -122,9 +149,14 @@ class ContactsSink(DonorPerfectSink):
     def upsert_record(self, record: dict, context: dict) -> None:
         """Upsert the record."""
         method = "GET"
+        state_updates = dict()
+        contact_id = record.pop("contact_id", None)
 
-        # send request
         response = self.request_api(method, params=record)
         res_json = self.parse_xml_response(response.text)
+        if contact_id:
+            state_updates['is_updated'] = True
+            return contact_id, True, state_updates
+
         id = res_json.get("", None)
-        return id, True, dict()
+        return id, True, state_updates
